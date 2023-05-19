eb2a592354d65cffc539fe7ffd3c45ac5c0a66c7
Maksim Remin <reminmax@gmail.com>
2023-05-20 00:15:27 +0300

    Retrofit & API service DI.

---
+++ app/src/main/java/com/reminmax/pointsapp/di/NetworkModule.kt
@@ -0,0 +1,67 @@
+package com.reminmax.pointsapp.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.reminmax.pointsapp.common.util.BASE_URL
+import com.reminmax.pointsapp.common.util.CONNECT_TIMEOUT
+import com.reminmax.pointsapp.data.data_source.remote.PointsApiService
+import com.reminmax.pointsapp.data.network.NetworkResultCallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor { message ->
+            Timber.tag("OkHttp").d(message)
+        }.also {
+            it.setLevel(HttpLoggingInterceptor.Level.BODY)
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providePointsApiService(retrofit: Retrofit): PointsApiService {
+        return retrofit.create(PointsApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        val contentType = "application/json".toMediaType()
+        val json = Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addCallAdapterFactory(NetworkResultCallAdapterFactory.create())
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+    }
+
+}
