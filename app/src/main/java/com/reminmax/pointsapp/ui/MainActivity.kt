ee4c2c3a8e95348f562df0f9ab9739958dc30707
Maksim Remin <reminmax@gmail.com>
2023-05-21 00:06:39 +0300

    Удален неиспользуемый код, добавлен вызов RootNavigationGraph.

--- app/src/main/java/com/reminmax/pointsapp/ui/MainActivity.kt
+++ app/src/main/java/com/reminmax/pointsapp/ui/MainActivity.kt
@@ -3,44 +3,21 @@ package com.reminmax.pointsapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.reminmax.pointsapp.ui.navigation.RootNavigationGraph
 import com.reminmax.pointsapp.ui.theme.PointsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PointsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                RootNavigationGraph(
+                    navController = rememberNavController(),
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PointsAppTheme {
-        Greeting("Android")
     }
 }
