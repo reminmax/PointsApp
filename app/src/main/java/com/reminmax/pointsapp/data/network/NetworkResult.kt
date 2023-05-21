e857d18af2da6b64a4030bbe9e53f2533e5c856f
Maksim Remin <reminmax@gmail.com>
2023-05-21 12:01:42 +0300

    Добавлены расширения для удобства использования.

--- app/src/main/java/com/reminmax/pointsapp/data/network/NetworkResult.kt
+++ app/src/main/java/com/reminmax/pointsapp/data/network/NetworkResult.kt
@@ -5,3 +5,24 @@ sealed class NetworkResult<T : Any> {
     class Error<T : Any>(val code: Int, val message: String?) : NetworkResult<T>()
     class Exception<T : Any>(val e: Throwable) : NetworkResult<T>()
 }
+
+suspend fun <T : Any> NetworkResult<T>.onSuccess(
+    executable: suspend (T) -> Unit
+): NetworkResult<T> = apply {
+    if (this is NetworkResult.Success<T>)
+        executable(data)
+}
+
+suspend fun <T : Any> NetworkResult<T>.onError(
+    executable: suspend (code: Int, message: String?) -> Unit
+): NetworkResult<T> = apply {
+    if (this is NetworkResult.Error<T>)
+        executable(code, message)
+}
+
+suspend fun <T : Any> NetworkResult<T>.onException(
+    executable: suspend (e: Throwable) -> Unit
+): NetworkResult<T> = apply {
+    if (this is NetworkResult.Exception<T>)
+        executable(e)
+}
