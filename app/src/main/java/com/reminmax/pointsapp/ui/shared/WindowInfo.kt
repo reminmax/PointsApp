481e84c5dbf51cc1a9a4d3c86161098c5ef4abdb
Maksim Remin <reminmax@gmail.com>
2023-05-23 20:13:53 +0300

    Реафкторинг.

--- app/src/main/java/com/reminmax/pointsapp/ui/shared/WindowInfo.kt
+++ app/src/main/java/com/reminmax/pointsapp/ui/shared/WindowInfo.kt
@@ -5,18 +5,23 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+private const val COMPACT_SCREEN_WIDTH = 600
+private const val MEDIUM_SCREEN_WIDTH = 840
+private const val COMPACT_SCREEN_HEIGHT = 480
+private const val MEDIUM_SCREEN_HEIGHT = 900
+
 @Composable
 fun rememberWindowInfo(): WindowInfo {
     val configuration = LocalConfiguration.current
     return WindowInfo(
         screenWidthInfo = when {
-            configuration.screenWidthDp < 600 -> WindowInfo.WindowType.Compact
-            configuration.screenWidthDp < 840 -> WindowInfo.WindowType.Medium
+            configuration.screenWidthDp < COMPACT_SCREEN_WIDTH -> WindowInfo.WindowType.Compact
+            configuration.screenWidthDp < MEDIUM_SCREEN_WIDTH -> WindowInfo.WindowType.Medium
             else -> WindowInfo.WindowType.Expanded
         },
         screenHeightInfo = when {
-            configuration.screenHeightDp < 480 -> WindowInfo.WindowType.Compact
-            configuration.screenHeightDp < 900 -> WindowInfo.WindowType.Medium
+            configuration.screenHeightDp < COMPACT_SCREEN_HEIGHT -> WindowInfo.WindowType.Compact
+            configuration.screenHeightDp < MEDIUM_SCREEN_HEIGHT -> WindowInfo.WindowType.Medium
             else -> WindowInfo.WindowType.Expanded
         },
         screenWidth = configuration.screenWidthDp.dp,
