33330a83e62e75987b6c6a20c4254c670c82d839
Maksim Remin <reminmax@gmail.com>
2023-05-23 18:47:52 +0300

    ConnectivityManager DI.

--- app/src/main/java/com/reminmax/pointsapp/di/ApplicationModule.kt
+++ app/src/main/java/com/reminmax/pointsapp/di/ApplicationModule.kt
@@ -2,9 +2,11 @@ package com.reminmax.pointsapp.di
 
 import android.app.Application
 import android.content.Context
+import android.net.ConnectivityManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
@@ -13,5 +15,10 @@ object ApplicationModule {
 
     @Provides
     fun appContext(app: Application): Context = app
+
+    @Provides
+    fun provideConnectivityManager(@ApplicationContext context: Context): ConnectivityManager =
+        context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
 
 }
